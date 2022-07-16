529ba20b7995bc9644bac40c7a104ed0c434b565
valerianguivarch <vguivarch@appstud.com>
2022-07-16 15:14:03 +0200

    call 2

--- lib/view/modules/call/settings.dart
+++
@@ -1,3 +0,0 @@
-/// Define App ID and Token
-const APP_ID = "3eac4da1625d4bde816a6686c73e7b03";
-const Token = '0063eac4da1625d4bde816a6686c73e7b03IADnYjoW9fKG0+UJWQKr9TcyalWrYncukW5lf0gdIsdJF1g8qhwAAAAAEACQKMvtGZbRYgEAAQAYltFi';
